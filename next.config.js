f4b8720eb56a66cefb02e54e3ad889a54c640d13
irines <irina.kiselyowa12@gmail.com>
2023-04-14 23:53:21 +0300

    Banner card component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bayut-production.s3.eu-central-1.amazonaws.com',
+        port: '',
+        pathname: '/image/**',
+      },
+    ],
+  }
 }
 
 module.exports = nextConfig
